3d615417a0f6305b20ebd1a62799c9b280e96e90
{Yuichiro <{uichi6@gmail.com}>
2023-02-12 16:26:09 +0900

    33. 他のコンポーネントを使ってみる

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+</script>
+
+<template>
+<GuestLayout>
+    何か文字を書く
+</GuestLayout>
+</template>
