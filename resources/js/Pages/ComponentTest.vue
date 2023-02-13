73405120ecfa4d3a85e72ec3d7aa2d084d279c32
{Yuichiro <{uichi6@gmail.com}>
2023-02-13 23:09:37 +0900

    Input

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -1,6 +1,7 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue'
 import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
 </script>
 
 <template>
