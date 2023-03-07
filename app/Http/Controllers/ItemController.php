e014059d6fda402d346c6061423d77f09793d37a
{Yuichiro <{uichi6@gmail.com}>
2023-03-07 23:19:05 +0900

    55. Item Update 更新対応

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -46,7 +46,11 @@ class ItemController extends Controller
             'price' => $request->price,
         ]);
 
-        return to_route('items.index');
+        return to_route('items.index')
+        ->with([
+            'message' => '登録しました。',
+            'status' => 'success'
+        ]);
     }
 
     /**
@@ -57,7 +61,10 @@ class ItemController extends Controller
      */
     public function show(Item $item)
     {
-        //
+        // dd($item);
+        return Inertia::render('Items/Show',[
+            'item' => $item
+        ]);
     }
 
     /**
@@ -68,7 +75,9 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return Inertia::render('Items/Edit',[
+            'item' => $item
+        ]);
     }
 
     /**
@@ -80,7 +89,7 @@ class ItemController extends Controller
      */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        //
+        dd($item->name, $request->name);
     }
 
     /**
