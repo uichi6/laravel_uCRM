dd40d9abb239cac80e316d486cfe155f6d244e11
{Yuichiro <{uichi6@gmail.com}>
2023-03-09 18:59:03 +0900

    55. Item Update 更新対応

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -89,7 +89,18 @@ class ItemController extends Controller
      */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        dd($item->name, $request->name);
+        // dd($item->name, $request->name);
+        $item->name = $request->name;
+        $item->memo = $request->memo;
+        $item->price = $request->price;
+        $item->is_selling = $request->is_selling;
+        $item->save();
+
+        return to_route('items.index')
+        ->with([
+            'message' => '更新しました。',
+            'status' => 'success'
+        ]);
     }
 
     /**
--- resources/js/Pages/Items/Edit.vue
+++ resources/js/Pages/Items/Edit.vue
@@ -71,9 +71,9 @@ const updateItem = id => {
                                     <div class="p-2 w-full">
                                         <div class="relative">
                                             <label for="is_selling" class="leading-7 text-sm text-gray-600">ステータス</label>
-                                            <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" valur="1">
+                                            <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" value="1">
                                             <label class="ml-2 mr-4">販売中</label>
-                                            <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" valur="0">
+                                            <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" value="0">
                                             <label class="ml-2 mr-4">停止中</label>
                                             <div v-if="errors.is_selling">{{ errors.is_selling }}</div>
                                         </div>
