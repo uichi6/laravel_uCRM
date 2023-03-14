f3dfe999f9cc3b05dc4e853538c77d5fbb83b484
{Yuichiro <{uichi6@gmail.com}>
2023-03-14 15:06:11 +0900

    商品関連

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -2,11 +2,17 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head,Link } from '@inertiajs/inertia-vue3';
 import { nl2br } from '@/common'
+import { Inertia } from '@inertiajs/inertia'
 
 defineProps({
     item:Object
 })
 
+const deleteItem = id => {
+    Inertia.delete(route('items.destroy', { item: id }), {
+    onBefore: () => confirm('本当に削除しますか?')
+  })
+}
 </script>
 
 <template>
@@ -73,6 +79,11 @@ defineProps({
                                     <div class="p-2 w-full">
                                     <Link as="button" :href="route('items.edit', { item: item.id })" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</Link>
                                     </div>
+
+                                    <div class="mt-20 p-2 w-full">
+                                    <button @click="deleteItem(item.id)" class="flex mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">削除する</button>
+                                    </div>
+                                    
                                 </div>
                                 </div>
                             </div>
