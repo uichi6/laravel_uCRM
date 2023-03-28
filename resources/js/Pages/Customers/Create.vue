b4e2bfeecdaab21644f25d2e5a89f4c42e01fedd
{Yuichiro <{uichi6@gmail.com}>
2023-03-28 15:42:20 +0900

    郵便番号

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -3,15 +3,18 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
-
-defineProps({
-    errors:Object
-})
+import { Core as YubinBangoCore } from "yubinbango-core2";
 
 const form = reactive({
     name: null, kana: null, tel: null, email: null, postcode: null,
     address: null, birthday: null, gender: null, memo: null
 })
+
+const fetchAddress = () => {
+    new YubinBangoCore(String(form.postcode), (value) => {
+        console.log(value)
+    })
+}
 
 const storeCustomer = () => {
     Inertia.post('/customers', form)
@@ -74,7 +77,7 @@ const storeCustomer = () => {
                                     <div class="p-2 w-full">
                                         <div class="relative">
                                                 <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                                                <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                <input type="number" id="postcode" name="postcode" @change="fetchAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 <div v-if="errors.postcode">{{ errors.postcode }}</div>
                                         </div>
                                     </div>
