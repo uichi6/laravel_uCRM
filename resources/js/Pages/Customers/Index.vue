baf0726aae9272352bdeeff11c575faeda5fe369
{Yuichiro <{uichi6@gmail.com}>
2023-03-28 15:42:40 +0900

    郵便番号

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -14,7 +14,7 @@ const search = ref('')
 
 // ref の値を取得するには .valueが必要
 const searchCustomers = () => {
-    Inertia.get(route('customers.index', { search: search.value }))
+  Inertia.get(route('customers.index', { search: search.value }))
 }
 
 </script>
