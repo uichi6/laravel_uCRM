bd12aac5b5dd9dab2cc192104381d8e41d52c10d
{Yuichiro <{uichi6@gmail.com}>
2023-04-02 16:34:40 +0900

    72. 顧客情報をv-forで表示

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Purchase;
 
 class Item extends Model
 {
@@ -15,5 +16,12 @@ class Item extends Model
         'price',
         'is_selling',
     ];
+
+    public function purchases()
+    {
+        return $this->belongsToMany(Purchase::class)
+        ->withPivot('quantity');
+    }
+    
 
 }
---
+++ resources/js/Pages/Purchases/Create.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { getToday } from '@/common'
+import { onMounted, reactive } from 'vue'
+
+const props defineProps({
+    'customers': Array
+})
+
+
+onMounted(() => { // ページ読み込み後 即座に実行
+    form.date = getToday()
+})
+
+const form = reactive({ 
+    date: null
+    customer_id: null
+ })
+
+</script>
+<template>
+日付<br>
+<input type="date" name="date" v-model="form.date"><br>
+
+会員名<br>
+<select name="customer" v-model="form.customer_id">
+<option v-for="customer in
+customers" :value="customer.id" :key="customer.id">
+{{ customer.id }} : {{ customer.name }}
+</option>
+</select>
+
+</template>
