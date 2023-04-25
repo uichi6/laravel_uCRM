4ceb85a64e140d38ba7a7bca8da241a146326b79
{Yuichiro <{uichi6@gmail.com}>
2023-04-25 15:36:15 +0900

    90. 詳細画面 (ビュー側調整)

--- resources/js/Pages/Purchases/Show.vue
+++ resources/js/Pages/Purchases/Show.vue
@@ -90,11 +90,36 @@ onMounted(() => { // ページ読み込み後 即座に実行
                                         <div class="">
                                             <label for="price" class="leading-7 text-sm text-gray-600">合計金額</label><br>
                                             <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                                {{ totalPrice }} 円
+                                                {{ props.order[0].total }} 円
                                             </div>
                                             <div v-if="errors.tel">{{ errors.tel }}</div>
                                         </div>
                                     </div>
+
+                                    <div class="p-2 w-full">
+                                        <div class="">
+                                            <label for="price" class="leading-7 text-sm text-gray-600">ステータス</label><br>
+                                            <div v-if="props.order[0].staus == true" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                               未キャンセル
+                                            </div>
+                                            <div v-if="props.order[0].staus == false" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                               キャンセル済
+                                            </div>
+                                            <div v-if="errors.tel">{{ errors.tel }}</div>
+                                        </div>
+                                    </div>
+
+                                    <div class="p-2 w-full">
+                                        <div class="">
+                                            <label for="price" class="leading-7 text-sm text-gray-600">キャンセル日</label><br>
+                                            <div v-if="props.order[0].staus == false" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                {{ dayjs(props.order[0].updated_at).format('YYYY/MM/DD') }}
+                                            </div>
+                                            <div v-if="errors.tel">{{ errors.tel }}</div>
+                                        </div>
+                                    </div>
+
+
 
                                     <div class="p-2 w-full">
                                     <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
