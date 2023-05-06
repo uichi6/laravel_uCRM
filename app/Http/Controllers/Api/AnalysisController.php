8afe8d9f11554d244c2b42de04fdec1e1959a38c
{Yuichiro <{uichi6@gmail.com}>
2023-05-06 09:38:26 +0900

    105. サービスへの切り離し

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
+use App\Services\AnalysisService;
 
 class AnalysisController extends Controller
 {
@@ -15,18 +16,7 @@ class AnalysisController extends Controller
         $subQuery = Order::betweenDate($request->startDate, $request->endDate);
 
         if($request->type === 'perDay'){
-            $subQuery->where('status', true)
-            ->groupBy('id')
-            ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date'); 
-
-            $data = DB::table($subQuery)
-            ->groupBy('date')
-            ->selectRaw('date, sum(totalPerPurchase) as total')
-            ->get();
-
-            $labels = $data->pluck('date');
-            $totals = $data->pluck('total');
-
+            list($data, $labels, $totals) = AnalysisService::perDay($subQuery);
         }
         return response()->json([
             'data' => $data,
