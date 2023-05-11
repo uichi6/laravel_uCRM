64b972745a1a1fa00c08b9a9c4b99b423faf73de
{Yuichiro <{uichi6@gmail.com}>
2023-05-11 22:28:17 +0900

    113. API通信でデシル分析結果を取得

--- app/Services/DecileService.php
+++ app/Services/DecileService.php
@@ -1,52 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Inertia\Inertia;
-use App\Models\Order;
+namespace App\Services;
 use Illuminate\Support\Facades\DB;
 
-class AnalysisController extends Controller
+class DecileService
 {
-    public function index()
+    public static function decile($subQuery)
     {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
-        // $period = Order::betweenDate($startDate, $endDate)
-        // ->groupBy('id')
-        // ->selectRaw('id, customer_id,
-        // customer_name, status, created_at')
-        // ->orderBy('created_at')
-        // ->paginate(50);
-
-        // dd($period);
-
-        // $subQuery = Order::betweenDate($startDate, $endDate)
-        // ->where('status', true)
-        // ->groupBy('id')
-        // ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date'); 
-
-        // $data = DB::table($subQuery)
-        // ->groupBy('date')
-        // ->selectRaw('date, sum(totalPerPurchase) as total')
-        // ->get();
-
-        // dd($data);
-
-
-        return Inertia::render('Analysis');
-    }
-
-    public function decile()
-    {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
- 
         // 1. 購買ID毎にまとめる
-        $subQuery = Order::betweenDate($startDate, $endDate)
-        ->groupBy('id')
+        $subQuery = $subQuery->groupBy('id')
         ->selectRaw('id, customer_id, customer_name,
         SUM(subtotal) as totalPerPurchase');
 
@@ -131,7 +93,9 @@ class AnalysisController extends Controller
         ')
         ->get();
 
-        // dd($data);
+        $labels = $data->pluck('decile');
+        $totals = $data->pluck('totalPerGroup');
+
+        return [$data, $labels, $totals];
     }
 }
-
--- resources/js/Components/ResultTable.vue
+++ resources/js/Components/ResultTable.vue
@@ -25,4 +25,25 @@ const props = defineProps({
             </tbody>
         </table>
     </div>
+
+    <div v-if="data.type === 'decile' " class="lg:w-2/3 w-full mx-auto overflow-auto">
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+            <tr>
+                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">グループ</th>
+                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">平均</th>
+                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">合計金額</th>
+                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">構成比</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in data.data" :key="item.date">
+                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.decile }}</td>
+                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.average }}</td>
+                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.totalPerGroup }}</td>
+                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.totalRatio }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
