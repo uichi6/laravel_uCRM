785b8ce869b4351bc877813469f0ba7590df26e7
{Yuichiro <{uichi6@gmail.com}>
2023-05-13 14:31:13 +0900

    115. RFM分析2 RFMランクをつけてみる

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -11,8 +11,8 @@ class AnalysisController extends Controller
 {
     public function index()
     {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
+        $startDate = '2022-05-01';
+        $endDate = '2023-05-31';
 
         // $period = Order::betweenDate($startDate, $endDate)
         // ->groupBy('id')
@@ -34,6 +34,49 @@ class AnalysisController extends Controller
         // ->get();
 
         // dd($data);
+
+        //RFM分析
+        // 1. 購買ID毎にまとめる
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('id, customer_id, customer_name,
+        SUM(subtotal) as totalPerPurchase, created_at');
+
+        // 2. 会員毎にまとめて最終購入日、回数、合計金額を取得
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('customer_id, customer_name,
+        max(created_at) as recentDate,
+        datediff(now(), max(created_at)) as recency,
+        count(customer_id) as frequency,
+        sum(totalPerPurchase) as monetary')
+
+        // dd($subQuery);
+
+        // 4. 会員毎のRFMランクを計算
+        $subQuery = DB::table($subQuery);
+        ->selectRaw('customer_id, customer_name,
+        recentDate, recency, frequency, monetary,
+        case
+            when recency < 14 then 5
+            when recency < 28 then 4
+            when recency < 60 then 3
+            when recency < 90 then 2
+            else 1 end as r,
+        case
+            when 7 <= frequency then 5
+            when 5 <= frequency then 4
+            when 3 <= frequency then 3
+            when 2 <= frequency then 2
+            else 1 end as f,
+        case
+            when 300000 <= monetary then 5
+            when 200000 <= monetary then 4
+            when 100000 <= monetary then 3
+            when 30000 <= monetary then 2
+            else 1 end as m')->get();
+
+        dd($subQuery);
 
 
         return Inertia::render('Analysis');
